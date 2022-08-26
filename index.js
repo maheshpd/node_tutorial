c5e0731e7c7b91bfeaf9bfc17546c9d1a4a17e07
MaheshI2T <mahesh.prasad@ideas2tech.com>
2022-08-26 13:52:58 +0530

    second commit

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const arr = [2,3,4,5,6,3,7,8,89]
 //console.log(app.firstName+ ' And '+ app.lastName+ " " +app.z())
 // console.log(arr)
 
-/*
+
 let result = arr.filter((item)=>{
     return item === 3
 })
-*/
+
 
 let result = arr.filter((item) => {
     return item > 5
